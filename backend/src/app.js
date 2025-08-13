e547358cfe7be9594f98df343c4a9be901a79183
asalmeron3 <arturosalmeron24@gmail.com>
2025-08-13 00:07:54 -0500

    added initial get and post routes with tests

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,4 +5,26 @@ app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
+app.get("/api/thoughts", (req, res) => {
+  res.status(200).json([]);
+});
+
+app.get("/api/thought/:id", (req, res) => {
+  const id = Number(req.params.id);
+  // Simulate a database request
+  const thought = { id: id, content: "This is a thought from the database." };
+  res.status(200).json(thought);
+});
+
+app.post("/api/thought", (req, res) => {
+  const content = trim(req.body.content);
+  if (!content) {
+    return res.status(400).json({ error: "Content is required" });
+  } else {
+    // Simulate saving to the database
+    const newThought = { id: 88, content: content };
+    res.status(201).json(newThought);
+  }
+});
+
 module.exports = app;
--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -8,3 +8,29 @@ describe("GET /api/health", () => {
     expect(response.body).toEqual({ status: "ok" });
   });
 });
+
+describe("GET /api/thought", () => {
+  it("should return an array of thoughts from the db", async () => {
+    const response = await request(app).get("/api/thoughts");
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+});
+
+describe("GET /api/thought/:id", () => {
+  it("should return a single thought with the specified id", async() => {
+    const response = await request(app).get("/api/thought/88");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("id", 88);
+  });
+});
+
+describe("POST /api/thought", () => {
+  it("should create a new thought", async () => {
+    const newThought = { content: "This is a new thought." };
+    const response = await request(app).post("/api/thought").send(newThought);
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("content", newThought.content);
+  });
+});
